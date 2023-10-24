1fa4718095e779f8b08abfebc6672427a3632ea6
moguy93 <guymorgan@live.fr>
2023-10-24 09:31:34 +0200

    add exemple + exo

--- exemples/ts/dist/exemple2.js
+++ exemples/ts/dist/exemple2.js
@@ -22,7 +22,7 @@ console.log(message);
 // deux notations possibles pour declarer une liste d'objet
 var words = ['un', 'deux', 'trois'];
 var words2 = ['un', 'deux', 'trois'];
-words.push(1);
+// words.push(1);
 words.push('1');
 // exemple de spread operator
 var fruits = ["apple", "orange", "cherry"];
@@ -32,3 +32,23 @@ var fruitsCopy = __spreadArrays(fruits);
 var person = { firstName: "john", lastName: "Vincent" };
 var details = { age: 25, nationality: "American" };
 var personDetails = __assign(__assign({}, person), details);
+// exemple affectation destructuree + valeur par default
+var utilisateurs = [
+    { id: 1, nom: "Alice", email: "alice@gmail.com", age: 25 },
+    { id: 2, nom: "Alice2", email: "alice2@gmail.com", age: 35 },
+    { id: 3, nom: "Alice3", email: "alice3@gmail.com" },
+];
+function afficheUtilisateur(_a) {
+    var id = _a.id, nom = _a.nom, email = _a.email, _b = _a.age, age = _b === void 0 ? "non specifie" : _b;
+    // afficher les infos
+}
+// rest et spread
+// rest
+function triUtilsateurs() {
+    var ids = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        ids[_i] = arguments[_i];
+    }
+    //code ici
+    return 0;
+}
--- exemples/ts/exemple2.ts
+++ exemples/ts/exemple2.ts
@@ -7,7 +7,7 @@ console.log(message)
 const words: Array<string> = ['un', 'deux', 'trois']
 const words2: string[] = ['un', 'deux', 'trois']
 
-words.push(1);
+// words.push(1);
 words.push('1');
 
 // exemple de spread operator
@@ -23,3 +23,23 @@ let person: object = { firstName: "john", lastName: "Vincent"}
 let details: object = { age: 25, nationality: "American" }
 
 let personDetails: object = { ...person, ...details };
+
+// exemple affectation destructuree + valeur par default
+
+const utilisateurs: object[] = [
+    { id: 1, nom: "Alice", email: "alice@gmail.com", age: 25 },
+    { id: 2, nom: "Alice2", email: "alice2@gmail.com", age: 35 },
+    { id: 3, nom: "Alice3", email: "alice3@gmail.com" },
+]
+
+function afficheUtilisateur({id, nom, email, age = "non specifie"}) {
+    // afficher les infos
+}
+
+// rest et spread
+
+// rest
+function triUtilsateurs(...ids: number[]): number {
+    //code ici
+    return 0
+}
