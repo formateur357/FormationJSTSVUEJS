ab3b1e458373641612553143f6c6078a9df0639e
moguy93 <guymorgan@live.fr>
2023-10-25 11:50:53 +0200

    add exemple

--- exemples/vuejs/mon-projet/src/components/HelloWorld.vue
+++ exemples/vuejs/mon-projet/src/components/HelloWorld.vue
@@ -1,31 +1,17 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <input v-model="text">
+    <p>{{ text }}</p>
+    <button @click="toggleSeen">Toggle</button>
+    <h1 v-if="text === 'if'">{{ msg }}</h1>
+    <h1 v-else-if="text === 'elseif'">{{ msg }} !</h1>
+    <h1 v-else>{{ msg }} !!</h1>
+    <ul v-show="seen">
+      <!-- <li v-for="item in items">{{ item.text }}</li> -->
+      <div v-for="{name, id, color} in items" :key="id">
+        <p>Name: {{ name }}</p>
+        <p>Color: {{ color }}</p>
+      </div>
     </ul>
   </div>
 </template>
@@ -35,6 +21,20 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: {
+    items: [
+      {name: "Apple", id: 0, color: "red"},
+      {name: "Banana", id: 1, color: "yellow"},
+      {name: "Peach", id: 2, color: "pink"},
+    ],
+    seen: true,
+    text: "Texte"
+  },
+  methods: {
+    toggleSeen() {
+      this.seen = !this.seen
+    }
   }
 }
 </script>
