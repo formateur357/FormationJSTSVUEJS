42863689fa128d50567584f7516123a40fa8bfb7
moguy93 <guymorgan@live.fr>
2023-10-27 14:56:23 +0200

    add all

---
+++ vite/mon-projet/src/components/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about"> 
+      <h1>aboutComponent</h1>
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component({})
+  export default class AboutComponent extends Vue {
+
+    mounted (){
+      console.log('hello from app');
+    }
+    
+  }
+
+</script>
+
+<style scoped lang="scss">
+  .about {
+
+  }
+</style>
---
+++ vite/mon-projet/src/components/about/about.component.vue
@@ -0,0 +1,3 @@
+<template src="./about.component.html"></template>
+<script src="./about.component.ts" lang="ts"></script>
+<style src="./about.component.scss" scoped lang="scss"></style>
